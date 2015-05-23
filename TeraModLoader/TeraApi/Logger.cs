09dd84fc5af93cb799e995f1f0d2258ccaf1e52f
Detrav <witalyezep@gmail.com>
2015-05-23 11:04:53 +0300

    Added logger class

---
+++ TeraModLoader/TeraApi/Logger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Detrav.TeraModLoader.TeraApi
+{
+    public static class Logger
+    {
+        public static void log(string format,params object[] args )
+        {
+            Console.WriteLine("{0:yyyy MM dd HH:mm:ss} {1,8}:{2}",DateTime.Now,"[Log]",String.Format(
+                format,args));
+        }
+
+        public static void debug(string format, params object[] args)
+        {
+            Console.WriteLine("{0:yyyy MM dd HH:mm:ss} {1,8}:{2}", DateTime.Now, "[Debug]", String.Format(
+                format, args));
+        }
+
+        public static void info(string format, params object[] args)
+        {
+            Console.WriteLine("{0:yyyy MM dd HH:mm:ss} {1,8}:{2}", DateTime.Now, "[Info]", String.Format(
+                format, args));
+        }
+    }
+}
