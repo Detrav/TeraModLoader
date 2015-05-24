945115e1afab03afc5becc46c9f68db0b18f7c0d
Detrav <witalyezep@gmail.com>
2015-05-24 18:07:08 +0300

    Added OpCode version enum and not tracert files

---
+++ TeraApi/Events/PacketArrivalEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Detrav.TeraApi.Events
+{
+    public class PacketArrivalEventArgs : EventArgs
+    {
+        public TeraPacketWithData packet { get; private set; }
+        public PacketArrivalEventArgs(TeraPacketWithData packet)
+        {
+            this.packet = packet;
+        }
+    }
+    public delegate void OnPacketArrival(object sender, PacketArrivalEventArgs e);
+}
