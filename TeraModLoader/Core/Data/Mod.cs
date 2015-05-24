3196b6714c576d6c38c2e941b361ca30f744f302
Detrav <witalyezep@gmail.com>
2015-05-24 18:04:36 +0300

    Latest incomprehensible changes

--- TeraModLoader/Core/Data/Mod.cs
+++ TeraModLoader/Core/Data/Mod.cs
@@ -32,11 +32,24 @@ namespace Detrav.TeraModLoader.Core.Data
                 if (v.GetInterfaces().Contains(typeof(ITeraMod)))
                 {
                     if (v.GetMethod("getModIcon") != null)
-                        icon = (BitmapImage)v.GetMethod("getModIcon").Invoke(null, null);
+                        icon = ToImage((byte[])v.GetMethod("getModIcon").Invoke(null, null));
                     type = v;
                     ready = true;
                     break;
                 }
+            }
+        }
+
+        public BitmapImage ToImage(byte[] array)
+        {
+            using (var ms = new System.IO.MemoryStream(array))
+            {
+                var image = new BitmapImage();
+                image.BeginInit();
+                image.CacheOption = BitmapCacheOption.OnLoad; // here
+                image.StreamSource = ms;
+                image.EndInit();
+                return image;
             }
         }
 
