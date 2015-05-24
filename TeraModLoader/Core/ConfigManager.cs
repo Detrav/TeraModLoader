454eb4a36f9b6ed4187ded8c712afe853bdd7a77
Detrav <witalyezep@gmail.com>
2015-05-24 11:59:28 +0300

    Added configManager

---
+++ TeraModLoader/Core/ConfigManager.cs
@@ -0,0 +1,42 @@
+﻿using Detrav.TeraModLoader.TeraApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Detrav.TeraModLoader.Core
+{
+    class ConfigManager : IConfigManager
+    {
+        string file = "config";
+        string modName = "unknown";
+
+        public ConfigManager(string modName)
+        {
+            this.modName = modName;
+        }
+
+        public void init(string playerName = null)
+        {
+            if(playerName == null)
+            {
+                file = Path.Combine(file, "config.json");
+                return;
+            }
+
+            file = Path.Combine(file, playerName, String.Format("{0}.json", modName));
+        }
+
+        public void save(object config)
+        {
+            
+        }
+
+        public object load()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TeraModLoader/TeraApi/Interfaces/IConfigManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Detrav.TeraModLoader.TeraApi.Interfaces
+{
+    interface IConfigManager
+    {
+        void init(string playerName);
+        void save(object config);
+        object load();
+    }
+}
