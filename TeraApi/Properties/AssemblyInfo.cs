5c6aa6bbfcb1de534e3c34ea657cf77902ec59b9
Detrav <witalyezep@gmail.com>
2015-05-29 16:52:56 +0300

    Сделал автоназначение версии

--- TeraApi/Properties/AssemblyInfo.cs
+++ TeraApi/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("TeraApi")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("HOME")]
 [assembly: AssemblyProduct("TeraApi")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.0.{GitCommitsCount}")]
+[assembly: AssemblyFileVersion("0.2.0.{GitCommitsCount}")]
