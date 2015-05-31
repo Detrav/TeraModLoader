2f333ee52d871eb03b9c1401e791446e796f6e7f
Detrav <witalyezep@gmail.com>
2015-05-31 21:25:29 +0300

    Add new packet

---
+++ TeraApi/OpCodes/P2904/S_SPAWN_NPC.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Detrav.TeraApi.OpCodes.P2904
+{
+    public class S_SPAWN_NPC : TeraPacket
+    {
+        public ulong id { get; set; }
+        public ulong parentId { get; set; }
+        public S_SPAWN_NPC(TeraPacketWithData packet)
+            : base(packet)
+        {
+            id = packet.toUInt64(8);
+            parentId = packet.toUInt64(85);
+        }
+    }
+}
