b2caf8d48382080545082f8f3f53c4657bff155f
Detrav <witalyezep@gmail.com>
2015-06-04 18:02:17 +0300

    Add namespace

--- TeraApi/Core/TeraPlayer.cs
+++ TeraApi/Core/TeraPlayer.cs
@@ -1,19 +1,21 @@
-
-public class TeraPlayer
+namespace Detrav.TeraApi.Core
 {
-	ulong id {get;set;}
-	string name{get;set;}
-	ulong partyId{get;set;}
-	uint hp{get;set;}
-	uint maxHp{get;set;}
-	uint mp{get;set;}
-	uint maxMp{get;set;}
-	uint stamina{get;set;}
-	uint maxStamina{get;set;}
-	
-	public TeraPlayer(ulong id,string name)
-	{
-		this.id = id;
-		this.name = name;
-	}
+    public class TeraPlayer
+    {
+        ulong id { get; set; }
+        string name { get; set; }
+        ulong partyId { get; set; }
+        uint hp { get; set; }
+        uint maxHp { get; set; }
+        uint mp { get; set; }
+        uint maxMp { get; set; }
+        uint stamina { get; set; }
+        uint maxStamina { get; set; }
+
+        public TeraPlayer(ulong id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+    }
 }
