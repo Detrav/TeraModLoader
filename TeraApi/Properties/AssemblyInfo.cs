63b45834ffc33f9d80734c70ba46096f27c20ba3
Detrav <witalyezep@gmail.com>
2015-06-05 01:44:59 +0300

    RC 4.0

--- TeraApi/Properties/AssemblyInfo.cs
+++ TeraApi/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.2.{GitCommitsCount}")]
-[assembly: AssemblyFileVersion("0.3.2.{GitCommitsCount}")]
+[assembly: AssemblyVersion("0.4.0.{GitCommitsCount}")]
+[assembly: AssemblyFileVersion("0.4.0.{GitCommitsCount}")]
