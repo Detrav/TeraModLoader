40ffa4ae4491c4371b6033cf47d31bbc06e6c9cd
Detrav <witalyezep@gmail.com>
2015-06-04 15:43:56 +0300

    добавил ещё 3 пакета

---
+++ TeraApi/OpCodes/P2904/S_PARTY_MEMBER_CHANGE_HP.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Detrav.TeraApi.OpCodes.P2904
+{
+    public class S_PARTY_MEMBER_CHANGE_HP : TeraPacket
+    {
+        ulong partyId;
+        uint value;
+        uint max;
+        public S_PARTY_MEMBER_CHANGE_HP(TeraPacketWithData packet)
+            : base(packet)
+        {
+            partyId = packet.toUInt64(4);
+            value = packet.toUInt32(12);
+            max = packet.toUInt32(16);
+        }
+    }
+}
---
+++ TeraApi/OpCodes/P2904/S_PARTY_MEMBER_CHANGE_STAMINA.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Detrav.TeraApi.OpCodes.P2904
+{
+    public class S_PARTY_MEMBER_CHANGE_STAMINA :TeraPacket
+    {
+        ulong partyId;
+        uint value;
+        uint max;
+        public S_PARTY_MEMBER_CHANGE_STAMINA(TeraPacketWithData packet) : base(packet)
+        {
+            partyId = packet.toUInt64(4);
+            value = packet.toUInt32(12);
+            max = packet.toUInt32(16);
+        }
+    }
+}
