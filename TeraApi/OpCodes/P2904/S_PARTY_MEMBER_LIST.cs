c88d767633275194fca7c966adce57924472d7c5
Detrav <witalyezep@gmail.com>
2015-06-04 15:35:14 +0300

    Add _partyId to S_PARTY_MEMBER_LIST

--- TeraApi/OpCodes/P2904/S_PARTY_MEMBER_LIST.cs
+++ TeraApi/OpCodes/P2904/S_PARTY_MEMBER_LIST.cs
@@ -19,12 +19,13 @@ namespace Detrav.TeraApi.OpCodes.P2904
             for(int i = 0; i< count; i++)
             {
                 ushort nameStart = packet.toUInt16(current);
+                ulong _partyId = packet.toUInt64(current + 2);
                 ulong _id = packet.toUInt64(current + 19);
                 ushort _level = packet.toUInt16(current + 10);
                 PlayerClass _playerClass = (PlayerClass)packet.toByte(current + 14);
                 string _name = packet.toDoubleString(nameStart, nameStart + 100);
                 current = nameStart + (_name.Length + 1) * 2 + 4;
-                list.Add(new Player() { id = _id, name = _name,level = _level, playerClass= _playerClass });
+                list.Add(new Player() { id = _id, name = _name, level = _level, playerClass = _playerClass, partyId = _partyId });
             }
             players = list.ToArray();
         }
@@ -32,6 +33,7 @@ namespace Detrav.TeraApi.OpCodes.P2904
         public class Player
         {
             public ulong id { get; set; }
+            public ulong partyId { get; set; }
             public string name { get; set; }
             public ushort level { get; set; }
             public PlayerClass playerClass { get; set; }
