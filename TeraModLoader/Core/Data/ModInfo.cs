47b02199b2ce9c368a5768a2ab9280c4df97993c
Detrav <witalyezep@gmail.com>
2015-06-10 00:48:36 +0300

    Добавил механику проверки версии

--- TeraModLoader/Core/Data/ModInfo.cs
+++ TeraModLoader/Core/Data/ModInfo.cs
@@ -20,7 +20,89 @@ namespace Detrav.TeraModLoader.Core.Data
 
         internal bool inVersion(System.Version version)
         {
-            throw new NotImplementedException();
+            if (Version == null) return false;
+            var ver = Version.Split('.');
+            if(ver.Length>0)
+            {
+                switch(ver[0].Trim()[0])
+                {
+                    case '<':
+                        { 
+                        int val;
+                        if (!int.TryParse(ver[0].Trim().Substring(1).Trim(), out val)) return false;
+                        if (version.Major >= val) return false;
+                        }
+                        break;
+                    case '>':
+                        {
+                            int val;
+                            if (!int.TryParse(ver[0].Trim().Substring(1).Trim(), out val)) return false;
+                            if (version.Major <= val) return false;
+                        }
+                        break;
+                }
+                if(ver.Length>1)
+                {
+                    switch (ver[1].Trim()[0])
+                    {
+                        case '<':
+                            {
+                                int val;
+                                if (!int.TryParse(ver[1].Trim().Substring(1).Trim(), out val)) return false;
+                                if (version.Minor >= val) return false;
+                            }
+                            break;
+                        case '>':
+                            {
+                                int val;
+                                if (!int.TryParse(ver[1].Trim().Substring(1).Trim(), out val)) return false;
+                                if (version.Minor <= val) return false;
+                            }
+                            break;
+                    }
+                    if (ver.Length > 2)
+                    {
+                        switch (ver[2].Trim()[0])
+                        {
+                            case '<':
+                                {
+                                    int val;
+                                    if (!int.TryParse(ver[2].Trim().Substring(1).Trim(), out val)) return false;
+                                    if (version.Build >= val) return false;
+                                }
+                                break;
+                            case '>':
+                                {
+                                    int val;
+                                    if (!int.TryParse(ver[2].Trim().Substring(1).Trim(), out val)) return false;
+                                    if (version.Build <= val) return false;
+                                }
+                                break;
+                        }
+                    }
+                    if (ver.Length > 3)
+                    {
+                        switch (ver[3].Trim()[0])
+                        {
+                            case '<':
+                                {
+                                    int val;
+                                    if (!int.TryParse(ver[3].Trim().Substring(1).Trim(), out val)) return false;
+                                    if (version.Revision >= val) return false;
+                                }
+                                break;
+                            case '>':
+                                {
+                                    int val;
+                                    if (!int.TryParse(ver[3].Trim().Substring(1).Trim(), out val)) return false;
+                                    if (version.Revision <= val) return false;
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+            return true;
         }
     }
 }
