326e53b3148f5f5a30daa620c86fb7a3ae607726
Detrav <witalyezep@gmail.com>
2015-06-10 12:49:14 +0300

    Add class for combobox enums

---
+++ TeraModLoader/Core/Data/ComboBoxEnumWithDescription.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Detrav.TeraModLoader.Core.Data
+{
+    class ComboBoxEnumWithDescription
+    {
+        private string text;
+        public object e { get; private set; }
+        public ComboBoxEnumWithDescription(object e)
+        {
+            FieldInfo fi = e.GetType().GetField(e.ToString());
+
+            if (null != fi)
+            {
+                object[] attrs = fi.GetCustomAttributes(typeof(DescriptionAttribute), true);
+                if (attrs != null && attrs.Length > 0)
+                {
+                    text = ((DescriptionAttribute)attrs[0]).Description;
+                    return;
+                }
+            }
+            text = null;
+        }
+
+        public override string ToString()
+        {
+            if (text != null) return text;
+            return base.ToString();
+        }
+    }
+}
