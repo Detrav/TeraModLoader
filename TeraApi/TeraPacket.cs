d1ec1e29efac2f4793aa77b7bc5095e441abaeac
Detrav <witalyezep@gmail.com>
2015-06-12 13:21:59 +0300

    Create hard to string debug

--- TeraApi/TeraPacket.cs
+++ TeraApi/TeraPacket.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -22,7 +24,36 @@ namespace Detrav.TeraApi
 
         public override string ToString()
         {
-            return base.ToString();
+            StringBuilder sb = new StringBuilder();
+            PrintProperties(sb, this, 1);
+            return sb.ToString();
+        }
+
+        public void PrintProperties(StringBuilder sb, object obj, int indent)
+        {
+
+            if (obj == null) return;
+            string indentString = new string(' ', indent);
+            Type objType = obj.GetType();
+            PropertyInfo[] properties = objType.GetProperties();
+            foreach (PropertyInfo property in properties)
+            {
+                object propValue = property.GetValue(obj, null);
+                if (property.PropertyType.IsPrimitive || property.PropertyType == typeof(string))
+                    sb.AppendLine(String.Format("{0}{1}: {2}", indentString, property.Name, propValue));
+                else if (typeof(IEnumerable).IsAssignableFrom(property.PropertyType))
+                {
+                    sb.AppendLine(String.Format("{0}{1}:", indentString, property.Name));
+                    IEnumerable enumerable = (IEnumerable)propValue;
+                    foreach (object child in enumerable)
+                        PrintProperties(sb, child, indent + 2);
+                }
+                else
+                {
+                    sb.AppendLine(String.Format("{0}{1}:", indentString, property.Name));
+                    PrintProperties(sb, propValue, indent + 2);
+                }
+            }
         }
     }
 }
