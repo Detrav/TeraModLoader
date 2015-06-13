9f58ab03486cb5a89de443089a331cf772e2ee4b
Detrav <witalyezep@gmail.com>
2015-06-13 12:43:42 +0300

    Сформировал заглушки в логгер файле

--- TeraModLoader/Core/LoggerFile.cs
+++ TeraModLoader/Core/LoggerFile.cs
@@ -1,11 +1,29 @@
-﻿using System;
+﻿using Detrav.TeraApi.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace Detrav.TeraModLoader.Core
 {
-    class LoggerFile
+    class LoggerFile : ILoggerFile
     {
+        private string file;
+
+        public LoggerFile(string file)
+        {
+            // TODO: Complete member initialization
+            this.file = file;
+        }
+
+        public void log(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
