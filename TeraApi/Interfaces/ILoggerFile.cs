761d3268c47ac879c9654628ce7d5cec72656b24
Detrav <witalyezep@gmail.com>
2015-06-13 12:37:19 +0300

    Create logger interface

---
+++ TeraApi/Interfaces/ILoggerFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Detrav.TeraApi.Interfaces
+{
+    interface ILoggerFile : IDisposable
+    {
+        /// <summary>
+        /// Записываем в лог текст, каждая запись в новой строке
+        /// </summary>
+        /// <param name="text">Текст для записи</param>
+        void log(string text);
+    }
+}
